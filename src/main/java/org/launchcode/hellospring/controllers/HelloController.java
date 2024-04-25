3a8f46d472a0099ab036acd8e4f7299efa58f1d4
June Lee <junelee@Junes-Laptop.local>
2024-04-25 17:04:38 -0500

    Requst Parameter

--- src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -10,16 +10,23 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class HelloController {
 
     // Handles request at path /hello
-    @GetMapping("hello")
-    @ResponseBody
-    public String hello() {
-        return "Hello, Spring!";
-    }
+//    @GetMapping("hello")
+//    @ResponseBody
+//    public String hello() {
+//        return "Hello, Spring!";
+//    }
 
     @GetMapping("goodbye")
     @ResponseBody
     public String goodbye() {
         return "Peace-out, Spring!";
+    }
+
+    //Handler Rqst of the form /hello?name=LaunchCode
+    @GetMapping("hello")
+    @ResponseBody
+    public String helloWQueryParam(@RequestParam String name) {
+        return "Hello, " + name + "!";
     }
 }
 
