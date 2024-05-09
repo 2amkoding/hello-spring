4256621958d03b331aed2966fe35b2889f38e102
June Lee <junelee@Junes-Laptop.local>
2024-05-09 14:09:38 -0500

    testing

--- src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -3,30 +3,29 @@ package org.launchcode.hellospring.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-
 public class HelloController {
 
-    // Handles request at path /hello
-//    @GetMapping("hello")
-//    @ResponseBody
-//    public String hello() {
-//        return "Hello, Spring!";
-//    }
+    //Handles request at PATH /hello
+    @GetMapping("hello")
+    @ResponseBody
+    public String hello() {
+        return "Hello, Spring!";
+    }
 
     @GetMapping("goodbye")
     @ResponseBody
     public String goodbye() {
-        return "Peace-out, Spring!";
+        return "Peace, Spring!";
     }
 
-    //Handler Rqst of the form /hello?name=LaunchCode
-    @GetMapping("hello")
+    //Handles DYNAMIC rqsts of the form /song?name=BBLDRIZZY
+    @GetMapping("song")
     @ResponseBody
-    public String helloWQueryParam(@RequestParam String name) {
-        return "Hello, " + name + "!";
+    public String helloWQueryParam(@RequestParam String name){
+        return "Hello, " + name + " !";
     }
 }
-
