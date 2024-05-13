2cfcba38a8f5fc9ff58ea96eb52330c82e4a41c1
June Lee <junelee@Junes-Laptop.local>
2024-05-12 21:36:35 -0500

    conditions in a template

--- src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -2,30 +2,48 @@ package org.launchcode.hellospring.controllers;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
-
-
 public class HelloController {
 
-    //Handles DYNAMIC rqsts of the form /hello?name=BBLDRIZZY
-    //Passes @RequesetParam as an argument of controller method
-    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST},value = "hello")
+    @GetMapping("goodbye")
     @ResponseBody
-    public String helloWQueryParam(@RequestParam String name){
-        return "Hello, " + name + " !";
+    public String goodbye() {
+        return "Goodbye, Spring!";
     }
 
-    //Hanldes rqsts of the form using PATH: /hello/LaunchCode
+    // Handles requests of the form /hello?name=LaunchCode
+    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST}, value = "hello")
+    public String helloWithQueryParam(@RequestParam String name, Model model) {
+        String greeting = "Hello, " + name +"~";
+        model.addAttribute("wsup", greeting);
+        return "hello1";
+    }
+
+    // Handles requests of the form /hello/LaunchCode
     @GetMapping("hello/{name}")
-    @ResponseBody
-    public String helloWPathParam(@PathVariable String name) {
-        return "Hello, " + name + "!";
+    public String helloWithPathParam(@PathVariable String name, Model model) {
+        String greeting = "shoooooo~~" + "Wsup " + name;
+        model.addAttribute("wsup", greeting);
+        return "hello1";
     }
 
     @GetMapping("form")
-    public String helloForm(){
-        return "form";
+    public String helloform() {
+        return "form1";
+    }
+    @GetMapping("hello-names")
+    public String helloNames(Model model) {
+        List<String> names = new ArrayList<>();
+        names.add("June");
+        names.add("July");
+        names.add("August");
+        model.addAttribute("names", names);
+        return "list-names";
     }
 }
